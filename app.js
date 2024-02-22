73698e5f5b0d4ed4a647707a7eb94b371303d33b
rakibulhasan044 <rhrakib044@gmail.com>
2024-02-22 15:51:22 +0600

    all done

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 let ticketSelected = 0;
 function setBackgroundColorById(elementId) {
-    let count = 0;
 
+    let count = 0;
     const element = document.getElementById(elementId);
     let seatleft = document.getElementById('seat-left');
     const totalSeat = parseInt(seatleft.innerText);
 
     let select = document.getElementById('selected-seat');
-
     const newTableRow = document.createElement('tr');
 
     if(!element.classList.contains('bg-green-400') && ticketSelected < 4) {
         element.classList.add('bg-green-400', 'text-white');
         ticketSelected ++;
         count++;
+
         newTableRow.className = 'text-center';
         newTableRow.id = `${elementId + elementId}`
         newTableRow.innerHTML = `
@@ -25,26 +25,30 @@ function setBackgroundColorById(elementId) {
         tbodyElement.appendChild(newTableRow);
     } 
     else if(ticketSelected === 4) {
-        alert('You have selected maximum number of tickets');
+        if(element.classList.contains('bg-green-400')){
+            element.classList.remove('bg-green-400', 'text-white');
+            ticketSelected -= 1;
+            count--;
+            
+            const parentElement = document.getElementById('tbody');
+            const childElement = document.getElementById(elementId + elementId); 
+            parentElement.removeChild(childElement);
+        }
+        else {
+            alert('You have selected maximum number of tickets');
+        }
     }
     else if(element.classList.contains('bg-green-400')){
         element.classList.remove('bg-green-400', 'text-white');
         ticketSelected -= 1;
         count--;
+
         const parentElement = document.getElementById('tbody');
         const childElement = document.getElementById(elementId + elementId); 
-
-        {
         parentElement.removeChild(childElement);
-        }
     }
 
     seatleft.innerText = totalSeat - count;
-
     select.innerText = ticketSelected;
-  
-    confirmTicket(ticketSelected);
     totalPrice(ticketSelected);
 }
-
-
--- utility.js
+++ utility.js
@@ -8,16 +8,6 @@ function totalPrice(ticketSelected) {
     return totalPrice;
 }
 
-function confirmTicket(seat) {
-    const number = document.getElementById('number');
-    const nmbr = parseInt(number.value);
-    const btn = document.getElementById('btn-enable');
-    
-    if (seat > 0) { 
-        btn.removeAttribute("disabled");
-    }
-}
-
 function grandTotal() {
     const totalPrice = document.getElementById('total-price');
     const daam = parseInt(totalPrice.innerText)
@@ -25,42 +15,37 @@ function grandTotal() {
     const grandPrice = document.getElementById('grand-total');
     const discount = document.getElementById('discount');
     const check = discount.value ;
-    console.log(check + typeof check)
 
     if(daam === 2200){
         const element = document.getElementById('gayeb');
         if(check === 'NEW15') {
             grandPrice.innerText = daam * 0.85;
             element.classList.add('hidden');
-
             const parentDiv = document.getElementById('last');
             const newDivElement = document.createElement('div');
             newDivElement.innerHTML = `
             <div class="flex justify-between py-2 text-[#030712] font-medium">
-                <h4>Total Price</h4>
-                    <h4>BDT <span id="total-price">330</span></h4>
+                <h4>Total Discount</h4>
+                    <h4>BDT <span id="">330</span></h4>
                 </div>
             `;
 
             const lastChild = parentDiv.lastElementChild;
             parentDiv.insertBefore(newDivElement, lastChild);
-
         }
         else if(check === 'Couple 20') {
             grandPrice.innerText = daam * 0.80;
             element.classList.add('hidden');
             const parentDiv = document.getElementById('last');
-
             const newDivElement = document.createElement('div');
             newDivElement.innerHTML = `
             <div class="flex justify-between py-2 text-[#030712] font-medium">
-                <h4>Total Price</h4>
-                    <h4>BDT <span id="total-price">440</span></h4>
+                <h4>Total Discount</h4>
+                    <h4>BDT <span id="">440</span></h4>
                 </div>
             `;
 
             const lastChild = parentDiv.lastElementChild;
-
             parentDiv.insertBefore(newDivElement, lastChild);
         } 
     }
@@ -68,3 +53,19 @@ function grandTotal() {
         grandPrice.innerText = daam ;
     }
 }
+
+document.getElementById('number').addEventListener('keyup', function(event) {
+    const text = event.target.value;
+    const value = text.toString();
+    const deleteButton = document.getElementById('btn-enable');
+
+    const table = document.getElementById("tbl");
+    const rows = table.getElementsByTagName("tr");
+
+    if(value.length > 0 && rows.length > 1){
+        deleteButton.removeAttribute('disabled');
+    }
+    else {
+        deleteButton.setAttribute('disabled', true);
+    }
+})
